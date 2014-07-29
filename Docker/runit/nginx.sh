50e8c4acb671d794db1aa0edb5816355f0c606ff
dkirrane <dkirrane@avaya.com>
2014-07-29 18:27:12 +0100

    Kick off nginx

---
+++ Docker/runit/nginx.sh
@@ -0,0 +1,6 @@
+### In nginx.sh (make sure this file is chmod +x):
+#!/bin/sh
+# `/sbin/setuser root` runs the given command as the user `memcache`.
+# If you omit that part, the command will be run as root.
+exec /sbin/setuser root /usr/sbin/nginx >>/var/log/nginx.log 2>&1
+
