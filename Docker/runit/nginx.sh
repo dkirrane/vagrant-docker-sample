45849255202758482dbca6b8bc57de9aafdc009a
Desmond Kirrane <desmond.kirrane@gmail.com>
2014-08-05 00:37:14 +0100

    fixes

--- Docker/runit/nginx.sh
+++ Docker/runit/nginx.sh
@@ -3,5 +3,5 @@
 # `/sbin/setuser root` runs the given command as the user `memcache`.
 # If you omit that part, the command will be run as root.
 # exec /sbin/setuser root /usr/sbin/nginx >>/var/log/nginx.log 2>&1
-exec /usr/bin/nginx
+exec /sbin/setuser root /usr/sbin/nginx -c /etc/nginx/nginx.conf  -g "daemon off;"
 
